cb5a5e95ea83040615f29ff230a5a838a1302be6
Altmann, Lutz <lutz.altmann@barco.com>
2021-10-26 10:42:22 +0200

    added license / jenkinsfile platforms removed cmake stuff from conanfile addDefaultBarcoPlatforms
    bumped csbld to version 5.4.x

--- conanfile.py
+++ conanfile.py
@@ -4,22 +4,13 @@ import shutil
 class CmlConan(ConanFile):
     name = "cml"
     url = "https://git.barco.com/projects/BAT/repos/3rd_cml"
+    license = "Boost Software License - Version 1.0"
     description = "Vector math library"
-    generators = "cmake", "cmake_find_package"
-    options = {"shared": [True, False]}
-    default_options = {"shared": False}
     settings = "os", "compiler", "build_type", "arch"
     exports_sources = "cml/*", "CMakeLists.txt"
 
-
-    def build(self):
-        cmake_generator=None
-        if self.settings.os == "Windows":
-            cmake_generator="NMake Makefiles"
-        cmake = CMake(self, generator=cmake_generator)
-
-        cmake.configure()
-        cmake.build()
+    def package_id(self):
+        self.info.header_only()
 
     def package(self):
         self.copy("*.h", src="cml", dst="include/cml")
@@ -28,4 +19,3 @@ class CmlConan(ConanFile):
         self.cpp_info.filenames["cmake_find_package"] = "cml"
         self.cpp_info.names["cmake_find_package"] = "cml"
         self.cpp_info.includedirs = ["include"]
-
